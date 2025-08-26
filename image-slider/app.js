24d51687988db093e6ef59794d556b9f1284679d
buyath <buyaththayub@gmail.com>
2025-08-26 23:43:48 +0000

    feat : i just add few styling for image slider

--- image-slider/app.js
+++ image-slider/app.js
@@ -1,6 +1,5 @@
 import React,{useState} from "react";
 import ReactDOM from "react-dom/client";
-import { CountryCodes } from "validator/lib/isISO31661Alpha2";
 let images = [
     "https://media.architecturaldigest.com/photos/66a914f1a958d12e0cc94a8e/16:9/w_1280,c_limit/DSC_5903.jpg",
     "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -10,7 +9,8 @@ let images = [
 const ImageSlider = () => {
     const [count,setCount ]= useState(0);
     return (
-        <div>
+        <div className="image-slider">
+          <div >
             <img src={images[count]}></img>
             <div className="btn-container">
                 <button onClick={() => {
@@ -20,7 +20,7 @@ const ImageSlider = () => {
                     else{
                         setCount(count + 1);
                     }
-                }}>+</button>
+                }} className="left-btn">+</button>
                 <button onClick={() => {
                     if(count < 1){
                         setCount(0);
@@ -28,9 +28,10 @@ const ImageSlider = () => {
                     else{
                         setCount(count - 1);
                     }
-                }}>
+                }} className="right-btn">
                     -
                 </button>
+               </div>
             </div>
         </div>
     )
