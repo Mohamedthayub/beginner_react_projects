33e59fe08bf59bb38e52fe1d5f3fd670810f8141
buyath <buyaththayub@gmail.com>
2025-09-02 23:32:26 +0000

    feat(game): add logic for rock, paper, and scissor selection

--- rock-paper-scissor/app.js
+++ rock-paper-scissor/app.js
@@ -1,43 +1,34 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
 const choices = ["rock","paper","scissor"];
-function ComputerChoice(){
+function computerChoice(){
     const index = Math.floor(Math.random() * choices.length);
     return choices[index];
 }
-const Game = () => {
+function  Game  ()  {
     const [user, setUser] = useState(""); 
     const [userScore, setUserScore] = useState(0);
     const [computerScore, setComputerScore] = useState(0);
-    const [winner,showWinner] = useState("");
-    useEffect(() => {
-        const Computer = ComputerChoice();
-        if(user == Computer){
-            showWinner("Match is Draw");
+    const [winner,setWinner] = useState("");
+    function playground(userChoice){
+        const computer = computerChoice();
+        if(computer === userChoice){
+            setWinner("Match is Draw");
+            
+        }
+        else if((userChoice == "rock" && computer == "paper") || (userChoice == "paper" && computer == "scissor") || (userChoice == "scissor" && computer == "rock")){
+            setComputerScore(prev => prev + 1);
+            setWinner(`Computer Wins! Computer: ${computer}, You: ${userChoice}`);
         }
         else{
-            if(user == "rock" && Computer == "paper"){
-                setComputerScore(computerScore + 1);
-                showWinner(`Computer Wins computer Choice is ${Computer} user choice is ${user}`);
-            }
-            else if (user == "paper" && Computer == "scissor"){
-               setComputerScore(computerScore + 1);
-               showWinner(`Computer Wins computer Choice is ${Computer} user choice is ${user}`);
-
-               
-            }
-            else if (user == "scissor" && Computer == "rock"){
-                setComputerScore(computerScore + 1);
-                showWinner(`Computer Wins computer Choice is ${Computer} user choice is ${user}`);
-
-                
-            }
-            else{
-                setUserScore(userScore + 1);
-                showWinner(`user Wins user Choice is ${user} Computer choice is ${Computer}`);
-
-            }
+            setUserScore(prev => prev + 1);
+            setWinner(`You Win! You: ${userChoice}, Computer: ${computer}`);
+        }
+    }
+    useEffect(() => {
+        if(user){
+            playground(user);
         }
     },[user]);
     return (
@@ -48,20 +39,16 @@ const Game = () => {
                 <h1>computerScore : {computerScore}</h1>
             </div>
             <div className="image-container">
-                <img src="https://rockpaperscissoregame.netlify.app/images/img1.jpeg" onClick={() => setUser("rock")}></img>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcPmYxW8e7aHcwazHLUSFCXbhrui5ysttNPcoWXIrc1JBoU5JJ736k9T07ackM1RMt5SA&usqp=CAU" onClick={() => setUser("paper")}></img>
-                <img src="https://thumb.silhouette-ac.com/t/a7/a7c3020b4cfb4fd154c4fcfd62702df2_t.jpeg" onClick={() => setUser("scissor")}></img>
+                <img  alt= "rock" src="https://rockpaperscissoregame.netlify.app/images/img1.jpeg" onClick={() => setUser("rock")}></img>
+                <img  alt = "paper" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcPmYxW8e7aHcwazHLUSFCXbhrui5ysttNPcoWXIrc1JBoU5JJ736k9T07ackM1RMt5SA&usqp=CAU" onClick={() => setUser("paper")}></img>
+                <img  alt = "scissor" src="https://thumb.silhouette-ac.com/t/a7/a7c3020b4cfb4fd154c4fcfd62702df2_t.jpeg" onClick={() => setUser("scissor")}></img>
             </div>
         </div>
     )
 }
 
 const App = () => {
-    return(
-        <div>
-            <Game/>
-        </div>
-    )
+    return <Game/>
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
