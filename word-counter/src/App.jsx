7d4381c1a7eeb38bd99ac3ad4b0a9602b04aca6d
buyath <buyaththayub@gmail.com>
2025-09-08 18:17:28 +0000

    feat: complete word counter project with live word and character count

--- word-counter/src/App.jsx
+++ word-counter/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 const App = () => {
-  const [char ,setChar] = useState("");
-  const [word,setWord] = useState("");
+  const [character ,setCharacter] = useState("");
+  const [word,setWord] = useState([]);
+  function  wordCount (char){
+    let convertWord = char.split(" ");
+    let filteredWord = convertWord.filter((item) => item != "");
+    setWord(filteredWord);
+  }
    return (
-    <div className="main">
+    <div className="app">
       <h1>Word Counter</h1>
       <div className="input-container">
         <textarea cols={40} rows={10} onKeyUp={(e) =>  {
-         
+          setCharacter(e.target.value);
+          console.log(character);
+          wordCount(e.target.value);
         }} >
         </textarea>
-        <h1>Word Counts :{char.length} </h1>
+        <h1>Word Counts :{character.length} </h1>
         <h1>Character Counts :{word.length} </h1>
       </div>
     </div>
